e1ec9ac7233c7521fd8fab1e575b1af7a33161d3
arvind <arvind@aistmail.com>
2021-08-15 15:35:43 +0600

    добаивил программу для биндинга клавиш

---
+++ KeyBinder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PcInputX;
+
+namespace KeyBinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyboardX.Hook();
+            KeyboardX.KeyboardEvent += OnKeyboardEvent;
+
+            Application.Run();
+        }
+
+        private static void OnKeyboardEvent(object sender, KeyboardXEvent e)
+        {
+            if (e.Key != Keys.Insert) return;
+
+            if (XEvent.FromTransferData(e.ToTransferData()) is KeyboardXEvent xEvent)
+            {
+                xEvent.Key = Keys.B;
+                xEvent.KeyCode = 93;
+                xEvent.InjectNow();
+            }
+
+            e.StopEventPropagation = true;
+        }
+    }
+}
